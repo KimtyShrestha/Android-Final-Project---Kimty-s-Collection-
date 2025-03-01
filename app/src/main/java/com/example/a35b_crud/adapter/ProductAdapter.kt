b0f559344c671a133b2fefc3d8537773f51e9928
Kimti Shrestha <kimtyshrestha@gmail.com>
2025-03-02 01:21:22 +0545

    Kimty's Collcetion Initial Commit

---
+++ app/src/main/java/com/example/a35b_crud/adapter/ProductAdapter.kt
@@ -0,0 +1,66 @@
+package com.example.a35b_crud.adapter
+
+import android.content.Context
+import android.content.Intent
+import android.icu.text.Transliterator.Position
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.view.menu.MenuView.ItemView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.a35b_crud.R
+import com.example.a35b_crud.model.ProductModel
+import com.example.a35b_crud.ui.activity.UpdateProductActivity
+
+class ProductAdapter (val context :Context,
+    var data: ArrayList<ProductModel>):RecyclerView.Adapter<ProductAdapter.ProductViewHolder>()
+
+    { class ProductViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
+        val pName:TextView = itemView.findViewById(R.id.displayName)
+        val pPrice:TextView = itemView.findViewById(R.id.displayPrice)
+        val pDesc:TextView = itemView.findViewById(R.id.displayDesc)
+        val editButton:TextView = itemView.findViewById(R.id.lblEdit)
+
+
+    }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductViewHolder {
+            val itemView : View = LayoutInflater.from(context).inflate(R.layout.sample_products,parent,false)
+            return ProductViewHolder(itemView)
+        }
+
+        override fun getItemCount(): Int {
+            return data.size
+        }
+
+        override fun onBindViewHolder(holder: ProductViewHolder, position: Int) {
+            holder.pName.text=data[position].productName
+            holder.pPrice.text=data[position].price.toString()
+            holder.pDesc.text=data[position].productDesc
+
+            holder.editButton.setOnClickListener{
+                val intent = Intent (context,UpdateProductActivity::class.java)
+                intent.putExtra("productId",data[position].productId)
+                context.startActivity(intent)
+
+            }
+
+
+        }
+        fun updateData(products: List<ProductModel>){
+            data.clear()
+            data.addAll(products)
+            notifyDataSetChanged()}
+
+        fun  getProductId (position: Int):String{
+            return data[position].productId
+        }
+
+    }
+
+
+
+
+
+
---
+++ app/src/main/java/com/example/a35b_crud/model/ProductModel.kt
@@ -0,0 +1,10 @@
+package com.example.a35b_crud.model
+
+data class ProductModel (
+    var productId : String = "",
+    var productName : String = "",
+    var productDesc : String = "",
+    var price : Int = 0,
+    var url : String = "",
+){
+}
